dccabf1d56b877867faeb6f0c5823fb8b235b725
SON-HYUN <sctoa1@gmail.com>
2023-12-17 23:03:23 +0900

    누락 파일 추가

---
+++ src/main/java/com/classscheduler/Classscheduler/ClassschedulerApplication.java
@@ -0,0 +1,13 @@
+package com.classscheduler.Classscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassschedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassschedulerApplication.class, args);
+	}
+
+}
